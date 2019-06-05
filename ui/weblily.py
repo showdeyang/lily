0e6ad7d2009838f787ffae10adc223eb06e64592
showdeyang <showdeyang@gmail.com>
2019-06-05 09:16:38 +0800

    lily flask

---
+++ ui/weblily.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from flask import (Flask, render_template, request)
+import chatbot
+
+app = Flask("lily")
+
+@app.route("/")
+def hello():
+    return render_template('chat2.html')#, name=name
+
+@app.route('/', methods = ["POST"])
+def post():
+    query = request.data.decode()
+    
+    answer = chatbot.reply(query)
+    return answer
+
+app.run()
